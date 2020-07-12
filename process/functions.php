1215767ab2380abcbb910c90cf18e367b8e7f32c
unknown <geoffreyparrenas@gmail.com>
2020-07-12 17:27:43 +0800

    update

--- process/functions.php
+++ process/functions.php
@@ -15,39 +15,39 @@
             for($ctr = 0; $ctr < $resultCategoriesCount; $ctr++){
               $category = $resultCategories->fetch_assoc();
               echo '<br>';
-              echo '<h3 class="category" style="margin-left:20px; margin-top: 0px; color: white; text-align: center;">'.$category['category'].'</h3><br>';
+              echo '<h2 class="category" style="margin-left:20px; margin-top: 0px; color: white; text-align: center;">'.$category['category'].'</h2><br>';
 
 
               $selectProducts =
-              'SELECT img_dir, name, prices.price as prodPrice from products
+              'SELECT img_dir, name, prices.price as productPrice from products
                 INNER JOIN prices
                   ON prices.id = products.price_id
                 WHERE category_id ='.$category['id'].';';
-              $resultProducts = $db->query($selectProducts);
-              $resultProductsCount = $resultProducts->num_rows;
+              $result = $db->query($selectProducts);
+              $resultProductsCount = $result->num_rows;
 
 
               for($ctr2 = 0; $ctr2 < $resultProductsCount; $ctr2++){
-                $product = $resultProducts->fetch_assoc();
-                $productNameLink = strtolower(str_replace(' ', '-', $product['name']));
+                $product = $result->fetch_assoc();
+                $productLink = strtolower(str_replace(' ', '-', $product['name']));
                 $productName = $product['name'];
 
-          
+
                 echo '<div class="row display-item"
                       style="display: inline-block;
                             	margin-left:40px;
                             	margin-bottom: 30px;
                             	padding: 15px;
                             	background-color: orange;
-                            	border-radius: 20px;
+                            	border-radius: 5px;
                             	width: 19%;
                             	height: 40%;">';
                   echo '<center><h6 style="color:black; word-wrap: break-word; margin-top:0px;">'.$productName.'</h6></center>';
-                  echo '<a href="products/'.$productNameLink.'.php"> ';
+                  echo '<a href="products/'.$productLink.'.php"> ';
                     echo '<img class="item" src="'.$product['img_dir'].'" style="height: 150px; width: 180px;"><br>';
                   echo '</a>';
                   echo '<center><h5 style="color:black; margin-top:10px; margin-bottom:0px;">Price:</h5>
-                        <h6 style="color:black; margin-top:10px; margin-bottom:0px;"> PhP '.$product['prodPrice'].'.00 </h6></center>';
+                        <h6 style="color:black; margin-top:10px; margin-bottom:0px;"> PhP '.$product['productPrice'].'.00 </h6></center>';
                 echo '</div>';
               }
             }
